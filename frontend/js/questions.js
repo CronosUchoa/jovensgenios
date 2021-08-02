695384253bab9a95057a644de23043c52861edd4
Cronos Uchoa <Gabriel5432@gmail.com>
2021-08-02 02:54:42 -0300

    Criar as questões(hardcoded) e verificar se a resposta escolhida é a certa

---
+++ frontend/js/questions.js
@@ -0,0 +1,70 @@
+//aqui eu vou gerar as questões (hardcoded) - e apresentar "dinamicamente" no front
+// depois tratar a resposta -- depois vou tentar implementar o websocket(não faço ideia de como vou fazer isso).
+let questions = [
+    {
+        numb: 1,
+        question: "De quem é a famosa frase “Penso, logo existo”?",
+        answer:"c) Descartes",
+        options:
+        [
+            "a)Platão",
+            "b) Galileu Galilei",
+            "c) Descartes",
+            "d) Sócrates",
+            "e) Francis Bacon",
+        ]
+    },
+    {
+        numb: 2,
+        question: "Quais o menor e o maior país do mundo?",
+        answer:"a) Vaticano e Rússia",
+        options:
+        [
+            "a) Vaticano e Rússia",
+            "b) Nauru e China",
+            "c) Mônaco e Canadá",
+            "d) Malta e Estados Unidos",
+            "e) São Marino e Índia",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Qual foi o recurso utilizado inicialmente pelo homem para explicar a origem das coisas?",
+        answer:"e) A Mitologia",
+        options:
+        [
+            "a) A Filosofia",
+            "b) A Biologia",
+            "c) A Matemática",
+            "d) A Astronomia",
+            "e) A Mitologia",
+        ]
+    },
+    {
+        numb: 4,
+        question: "Quem foi o primeiro homem a pisar na Lua? Em que ano aconteceu?",
+        answer:"e) Neil Armstrong, em 1969.",
+        options:
+        [
+            "a) Yuri Gagarin, em 1961",
+            "b) Buzz Aldrin, em 1969",
+            "c) Charles Conrad, em 1969",
+            "d) Charles Duke, em 1971",
+            "e) Neil Armstrong, em 1969.",
+        ]
+    },
+    {
+        numb: 5,
+        question: " Quais são as fases da Lua?",
+        answer:"d) Nova, crescente, cheia e minguante",
+        options:
+        [
+            "a) Nova, cheia e superlua",
+            "b) Penumbral, lunar parcial, lunar total e cheia",
+            "c) Nova, cheia, minguante e lua de sangue",
+            "d) Nova, crescente, cheia e minguante",
+            "e) Nova, crescente, cheia, minguante e lua de sangue",
+        ]
+    }
+    
+];
--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -16,7 +16,70 @@ exit_btn.onclick = ()=>{
 }
 //se o botão continue for apertado
 continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); 
     quiz_box.classList.add("activeQuiz");
+    showQuestions(0);
+    queCounter(1);
 }
 
+
+//pegando as questões
+
+let que_count = 0;
+let que_numb = 1;
+
+const next_btn = quiz_box.querySelector(".next_btn");
+
+next_btn.onclick = () =>{
+    if(que_count < questions.length - 1){
+        que_count++;
+        que_numb++;
+        showQuestions(que_count);
+        queCounter(que_numb);
+
+    }
+    else{
+        console.log("questions completed");
+    }
+   
+}
+
+function showQuestions(index){
+    const que_text = document.querySelector(".que_text");
+    let que_tag = '<span>'+questions[index].numb+". "+ questions[index].question +'</span>';
+    
+
+    const option_list = document.querySelector(".option_list");
+    let option_tag = '<div class="option"><span>'+questions[index].options[0]+'</span></div>'
+                    +'<div class="option"><span>'+questions[index].options[1]+'</span></div>'
+                    +'<div class="option"><span>'+questions[index].options[2]+'</span></div>'
+                    +'<div class="option"><span>'+questions[index].options[3]+'</span></div>'
+                    +'<div class="option"><span>'+questions[index].options[4]+'</span></div>';
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+
+    const option = option_list.querySelectorAll(".option");
+    for(let i = 0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+//pegando a resposta do aluno. -- talvez seja aqui que eu tenha que implementar o websockt
+function optionSelected(answer){
+    let respostaAluno = answer.textContent;
+    let respostaCerta = questions[que_count].answer;
+    //verificar a resposta do aluno
+    if(respostaAluno == respostaCerta){
+        console.log("aluno acertou! " + respostaCerta);
+    }else{
+        console.log("aluno errou! " + respostaAluno) ;
+    }
+    
+}
+
+function queCounter(index){
+    const fo_quest_counter = quiz_box.querySelector(".total_que");
+    let totalQuesCountTag = '<span><p>'+ index +'</p>de<p>'+questions.length+'</p>Questões</span>';
+    fo_quest_counter.innerHTML = totalQuesCountTag;
+}
 
